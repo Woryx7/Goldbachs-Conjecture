9835e2073d40b5aa0bd0a1a90393998ed1675ede
Woryx7 <80184641+Woryx7@users.noreply.github.com>
2021-08-27 20:23:16 -0500

    Format update
    I added some comments, and changed the names of the variables to make it easier to understand. I also added a proper main guard to the code.

--- Goldbach Conjecture.py
+++ Goldbach Conjecture.py
@@ -1,28 +1,31 @@
 import math
+# cache variable to save known prime results
 known_primes = {1:False,2:True,3:True,4:False}
-
+# Function to find the sum of a number's digits
 def digitsum(number):
     digitsum = 0
     digits = [int(d) for d in str(number)]
     for i in range(len(digits)):
         digitsum += digits[i]
     return digitsum
+# Function to search for a number's divisors
 def sieve(number):
     limit = int(math.sqrt(number))
-    listn = {2}
-    primen = {2,3,5,7}
+    primes = {2}
+    basic_primes = {2,3,5,7}
     for i in range(2,limit+1):
-        listn.add(i)
-    for j in primen:
-        for k in primen:
-            if k % j == 0 and k not in primen:
+        primes.add(i)
+    for j in basic_primes:
+        for k in basic_primes:
+            if k % j == 0 and k not in basic_primes:
                 k = 0
-    while 0 in listn:
-        listn.discard(0)
-    for h in listn:
+    while 0 in primes:
+        primes.discard(0)
+    for h in primes:
         if number % h == 0:
             return False
     return True
+# Quick primality test based of the number's composition
 def isprime(number):
     if number in known_primes:
         return known_primes[number]
@@ -43,23 +46,26 @@ def isprime(number):
             result = sieve(number)
             known_primes[number] = result
             return result
-def iteration(ornum):
+# Function to find the sum of two prime numbers to give the original number
+def sum_search(number):
     golbach = False
-    for i in range(1,ornum):
-        if isprime(i)  and isprime(ornum-i):
-            print(ornum,"=",i,"+",ornum-i)
+    for i in range(1,number):
+        if isprime(i)  and isprime(number-i):
+            print(number,"=",i,"+",number-i)
             golbach = True
             break
-        if i >= int(ornum/2):
+        if i >= int(number/2):
             break
     if golbach == False:
         print("Goldbach's conjecture is wrong.")
+# Main part of the code
 def main():
     running = True
     while running:    
-        ornum = int(input())
-        if ornum != 0:
-            iteration(ornum)
+        number = int(input())
+        if number != 0:
+            sum_search(number)
         else:
             break
-main()
+if __name__ == '__main__':
+    main()
